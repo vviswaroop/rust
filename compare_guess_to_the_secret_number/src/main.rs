b7e3b4159f61f935b8bde2056f87c89556490633
Viswaroop Vadlamudi <viswaroopvadlamudi@Viswaroop.local>
2019-01-05 12:53:59 -0700

    compare_guess_to_the_secret_number

---
+++ compare_guess_to_the_secret_number/src/main.rs
@@ -0,0 +1,22 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
+
+    println!("You Gussed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+   }
+}
---
+++ compare_guess_to_the_secret_number/target/debug/deps/compare_guess_to_the_secret_number-29a0bf7f65e72b8b.d
@@ -0,0 +1,5 @@
+/Users/viswaroopvadlamudi/Desktop/rust/projects/rust/compare_guess_to_the_secret_number/target/debug/deps/compare_guess_to_the_secret_number-29a0bf7f65e72b8b: src/main.rs
+
+/Users/viswaroopvadlamudi/Desktop/rust/projects/rust/compare_guess_to_the_secret_number/target/debug/deps/compare_guess_to_the_secret_number-29a0bf7f65e72b8b.d: src/main.rs
+
+src/main.rs:
